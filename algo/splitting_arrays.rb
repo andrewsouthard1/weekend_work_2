0d67be6013b4bdbb3b33f3ee8c8cff5512f76e87
Andrew <andrewsouthard111@gmail.com>
2017-03-11 14:58:23 -0500

    Add while loop to splitting array, take count out of else loop

--- algo/splitting_arrays.rb
+++ algo/splitting_arrays.rb
@@ -1,7 +1,32 @@
 # Complete the method called split_array, which two parameters, the first being an array, and the second being an integer. The method will then create an array of other arrays, each one being the size of the indicated by the second parameter. See the driver code for examples.
 
 def split_array(array, array_size)
-
+  output_array = []
+  size_array = []
+  count = 0
+  while count <= array.length 
+    if size_array.length < array_size
+      size_array << array[count]
+      count += 1
+    else
+      output_array << size_array
+      size_array = []
+    end
+  end
+  output_array
+  # I had to hard code below to visualize it
+  # size_array << array[0]
+  # size_array << array[1]
+  # output_array << size_array
+  # size_array = []
+  # size_array << array[2]
+  # size_array << array[3]
+  # output_array << size_array
+  # size_array = []
+  # size_array << array[4]
+  # size_array << array[5]
+  # output_array << size_array
+  # output_array
 end
 
 # Driver code - don't touch anything below this line.
